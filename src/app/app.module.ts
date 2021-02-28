5bfd872f3711c3b622ad71bdde88447375bd2957
Ricardo Yaguachi <ricky081297@hotmail.com>
2021-02-27 22:30:23 -0500

    footer y contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
